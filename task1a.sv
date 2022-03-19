3532b13002df7641a7dd221c405b46fcb1d6bb5d
yanchaojin <98716365+yanchaojin@users.noreply.github.com>
2022-03-19 13:24:02 +0000

    Update task1a.sv

--- task1a.sv
+++ task1a.sv
@@ -16,5 +16,33 @@ A	B	C	Y
 module task1a(output logic [1:0] Y, input logic A, B, C);
 
 
+//internal node
+wire a1;
+wire a2;
+wire a3;
+wire a4;
+
+wire b1;
+wire b2;
+wire b3;
+wire b4;
+wire notA;
+wire notB;
+wire notC;
+not a_no (notA, A);
+not b_no (notB, B);
+not c_no (notC, C);
+and y0and1 (a1 ,notA,notB ,C   );
+and y0and2 (a2 ,notA,B,    notC );
+and y0and3 (a3 ,A,   notB, notC);
+and y0and4 (a4 ,A,   B,    C);
+or  Y0   (Y[0],a1,a2,a3,a4);
+
+and y1and1 (b1 ,notA,B ,   C   );
+and y1and2 (b2 ,A,   notB, C   );
+and y1and3 (b3 ,A,   B,    notC);
+and y1and4 (b4 ,A,   B,    C);
+or  Y1   (Y[1],b1,b2,b3,b4);
+
 
 endmodule
