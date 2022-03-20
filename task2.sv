84692c1f78613a14cbcd225c55d3bd3b84a4e3eb
yanchaojin <98716365+yanchaojin@users.noreply.github.com>
2022-03-20 13:11:07 +0000

    3.20

--- task2.sv
+++ task2.sv
@@ -1,7 +1,31 @@
 
 module task2 #(parameter N=16'b1010110011100001) (output logic [15:0] Q, input logic CLK, n_RESET);
 
-//Write solution here
+ always_ff @(posedge CLK,negedge n_RESET) begin
+    if(n_RESET == 1'b0)
+        begin
+            Q <= N; //reset for initial value
+        end 
+     else begin  //loop lsfr value
+          Q[0]    <= Q[1];
+          Q[1]    <= Q[2];
+          Q[2]    <= Q[3];
+          Q[3]    <= Q[4];
+          Q[4]    <= Q[5];
+          Q[5]    <= Q[6];
+          Q[6]    <= Q[7];
+          Q[7]    <= Q[8] ;
+          Q[8]    <= Q[9];
+          Q[9]    <= Q[10] ;
+          Q[10]   <= Q[11]^Q[0] ; //xor operation
+          Q[11]   <= Q[12];
+          Q[12]   <= Q[13]^Q[0] ;//xor operation
+          Q[13]   <= Q[14]^Q[0] ;//xor operation
+          Q[14]   <= Q[15];
+          Q[15]   <= Q[0];         
+       end 
+ end 
+ 
 
 
 endmodule
--- task2_tb.sv
+++ task2_tb.sv
@@ -1,11 +1,34 @@
 module task2_tb;
 
-// Add internal signals here
-
+logic [15:0] Q;
+logic CLK, n_RESET;
+logic [15:0] check_num = 0;
+task2 u1(Q,CLK,n_RESET);
 initial begin
+//check_num = 0;
+CLK = 0;
+n_RESET = 0;
+#50;
+n_RESET = 1;
+#20
+assert (Q == 57968) $display( "passed "); else $error("failed");
+#20
+assert (Q == 28984) $display( "passed "); else $error("failed");
+#20
+assert (Q == 14492) $display( "passed "); else $error("failed");
 
-//Write testbench here
-
+# (20*65533);
+assert (Q == 57968) $display( "passed "); else $error("failed");
+#20
+assert (Q == 28984) $display( "passed "); else $error("failed");
+#20
+assert (Q == 14492) $display( "passed "); else $error("failed");
 end
+
+always #10 CLK = ~CLK; 
+
+always_ff @ (posedge CLK)
+check_num <= check_num + 1;
+
 
 endmodule
