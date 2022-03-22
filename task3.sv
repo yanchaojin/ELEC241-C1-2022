4b9797d697ed71513387dbb17b3567b14665c7cb
yanchaojin <98716365+yanchaojin@users.noreply.github.com>
2022-03-22 18:28:14 +0000

    3.22

--- task3.sv
+++ task3.sv
@@ -1,6 +1,40 @@
 // Task 3 - 2022
 module task3;
 
-//Write solution here
+logic [3:0] count;
+logic [7:0] store;
+   always_ff @(posedge CLK) begin
+    if(START == 1'b1) begin   //reset for initial value
+            VALID <= 0;
+            Q     <= 0;
+            count <= 0;
+			store <= 8'b00000000;
+        end 
+     else begin 
+         if(count <=  7)begin            //max input number is 8,count form 0 to 7
+             count <= count + 1;         // count the input data number
+			 store[count] <= DATAIN;     //buffer input data
+		 end
+         else
+             count <= 7;  
+             
+         if(count <=  6)   begin
+             if(DATAIN == 0) begin       //if detect the first 0
+                 Q<= count;       
+                 VALID <= 1;
+             end
+             else begin    
+                 Q<= Q;
+                 VALID <= VALID;
+             end                   
+         end
+        else if(count == 7 && store == 8'b11111111)       //special situation,sequence is 11111111
+            begin
+                 Q<= 7;
+                 VALID <= 1;			
+            end			
+		 
+     end 
+ end 
 
 endmodule
--- task3_tb.sv
+++ task3_tb.sv
@@ -1,5 +1,56 @@
 module task3_tb;
 
-//Write testbench here
+logic [2:0] Q;
+logic  VALID;
+logic DATAIN,CLK, START;
+task3 u1(Q,VALID,CLK,DATAIN,START);
+initial begin
+DATAIN = 1;
+CLK = 0;
+START = 1;
+#110; START = 0; 
+     DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 0;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+
+#1000; START = 1;
+#100; START = 0;  
+     DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 0;
+#20  DATAIN = 1;
+#20  DATAIN = 0;
+
+#1000; START = 1;
+#100; START = 0;  
+     DATAIN = 0;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+
+#1000; START = 1;
+#100; START = 0;  
+     DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+#20  DATAIN = 1;
+end
+always #10 CLK = ~CLK; 
 
 endmodule
